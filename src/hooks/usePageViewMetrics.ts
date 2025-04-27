1592f1f8b8bc338743749f7a8e96922d2b1f8be8
crackcrickcrack <crackcrickcrack@gmail.com>
2025-04-27 09:28:24 +0530

    Update usePageViewMetrics.ts

--- src/hooks/usePageViewMetrics.ts
+++ src/hooks/usePageViewMetrics.ts
@@ -1,22 +1,15 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-/**
- * Hook to track page views for metrics
- * This would normally send data to a metrics service
- */
 export const usePageViewMetrics = () => {
   const location = useLocation();
   
   useEffect(() => {
-    // In a real implementation, this would send metrics to a service
+    // This would normally send metrics to a server
     // For now, we'll just log to console
     console.log(`Page view: ${location.pathname}`);
     
-    // This is where you would track metrics with a real service
-    // Example: trackPageView(location.pathname);
-    
+    // In a real implementation, you might do something like:
+    // trackPageView(location.pathname);
   }, [location.pathname]);
 };
-
-export default usePageViewMetrics;
